c6fdb59720ecc327a7d10de1022e79bc214323df
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 12:16:19 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/zup/GerenciadordeContas/GerenciadordeContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.GerenciadordeContas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordeContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordeContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/GerenciadordeContas/GerenciadordeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.GerenciadordeContas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
