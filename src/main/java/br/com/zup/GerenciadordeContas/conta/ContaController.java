cb73c2b8239465f427a12df653ef3d859e76c73a
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 13:31:13 -0300

    Adicionar anotações na Classe Controller.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/ContaController.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/ContaController.java
@@ -1,4 +1,9 @@
 package br.com.zup.GerenciadordeContas.conta;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contas")
 public class ContaController {
 }
