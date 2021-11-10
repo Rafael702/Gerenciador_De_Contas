74f7a0aa1b6b541e6076da447f2cb93c8b1e6728
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 13:26:44 -0300

    Adicionar anotação @Repository.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
@@ -2,7 +2,9 @@ package br.com.zup.GerenciadordeContas.conta.repository;
 
 import br.com.zup.GerenciadordeContas.conta.Conta;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContaRepository extends CrudRepository<Conta,Integer> {
 
 }
