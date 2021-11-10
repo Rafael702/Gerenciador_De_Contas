60449b2292325191331994c17238a363a34c34fe
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 13:37:49 -0300

    Fazer injeção de dependência da Inteface ContaRepository na ContaServico.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/servico/ContaServico.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/servico/ContaServico.java
@@ -1,8 +1,13 @@
 package br.com.zup.GerenciadordeContas.conta.servico;
 
+import br.com.zup.GerenciadordeContas.conta.repository.ContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContaServico {
+    @Autowired
+    private ContaRepository contaRepository;
+
 
 }
