ec7857d2d715a983a664add46620f9b173f70cb7
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 16:57:03 -0300

    Criar método salvar Contas.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/servico/ContaService.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/servico/ContaService.java
@@ -5,8 +5,11 @@ import br.com.zup.GerenciadordeContas.conta.repository.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Service
-public class ContaServico {
+public class ContaService {
     @Autowired
     private ContaRepository contaRepository;
 
