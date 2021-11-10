17b5290536b2d098a30db03b711f056895ff19f4
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 16:55:44 -0300

    Retirar a anotação, pois não é necessário.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
@@ -2,9 +2,7 @@ package br.com.zup.GerenciadordeContas.conta.repository;
 
 import br.com.zup.GerenciadordeContas.conta.Conta;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ContaRepository extends CrudRepository<Conta,Integer> {
+public interface ContaRepository extends CrudRepository<Conta, Integer> {
 
 }
