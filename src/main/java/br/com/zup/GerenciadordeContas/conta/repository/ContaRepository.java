abe5b6be574e1fe714cb19cae80f494b36fb3d61
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 13:21:34 -0300

    Herdar de CrudRepository.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
@@ -1,4 +1,7 @@
 package br.com.zup.GerenciadordeContas.conta.repository;
 
-public interface ContaRepository {
+import br.com.zup.GerenciadordeContas.conta.Conta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContaRepository extends CrudRepository<Conta,Integer> {
 }
