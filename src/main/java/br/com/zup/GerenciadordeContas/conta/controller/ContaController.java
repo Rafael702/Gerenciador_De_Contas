1ad02fa9bc40ac30f5a0db2dbe384edf5695cba2
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 16:53:59 -0300

    Incluir acesso ao Banco de Dados.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
@@ -1,9 +1,22 @@
 package br.com.zup.GerenciadordeContas.conta;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contas")
 public class ContaController {
+
+    @Autowired
+    private ContaServico contaServico;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarConta(Conta conta) {
+        contaServico.salvarConta(conta);
+    }
 }
--- src/main/java/br/com/zup/GerenciadordeContas/conta/servico/ContaService.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/servico/ContaService.java
@@ -1,5 +1,6 @@
 package br.com.zup.GerenciadordeContas.conta.servico;
 
+import br.com.zup.GerenciadordeContas.conta.Conta;
 import br.com.zup.GerenciadordeContas.conta.repository.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,8 @@ public class ContaServico {
     @Autowired
     private ContaRepository contaRepository;
 
+    public void salvarConta(Conta conta) {
+        contaRepository.save(conta);
+    }
 
 }
