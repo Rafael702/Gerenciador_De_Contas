00d97edbd7f09f356f33d68f79d3897e333d47d4
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 12:28:47 -0300

    Modelar Classe Conta.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/Conta.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/Conta.java
@@ -1,5 +1,67 @@
 package br.com.zup.GerenciadordeContas.conta;
 
-public class Conta {
+import br.com.zup.GerenciadordeContas.conta.enuns.Status;
+import br.com.zup.GerenciadordeContas.conta.enuns.Tipo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Conta {
+    private String nome;
+    private double valor;
+    private LocalDate dataDeVencimento;
+    private LocalDateTime dataDePagamento;
+    private Tipo tipo;
+    private Status status;
+
+    public Conta() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDateTime getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDateTime dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
