fb0c5e6242aca6d6c0d0cc94dbb0e29e63a403a6
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-10 16:57:40 -0300

    Criar método cadastrar Conta.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
@@ -1,22 +1,21 @@
-package br.com.zup.GerenciadordeContas.conta;
+package br.com.zup.GerenciadordeContas.conta.controller;
 
+import br.com.zup.GerenciadordeContas.conta.Conta;
+import br.com.zup.GerenciadordeContas.conta.servico.ContaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contas")
 public class ContaController {
 
     @Autowired
-    private ContaServico contaServico;
+    private ContaService contaService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void cadastrarConta(Conta conta) {
-        contaServico.salvarConta(conta);
+    public void cadastrarConta(@RequestBody Conta conta) {
+        contaService.salvarConta(conta);
     }
 }
