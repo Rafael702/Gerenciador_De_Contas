69db889e70bada08ed8f08e70b205613f2efbf03
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-12 18:13:19 -0300

    Incluir busca pelo tipo de conta.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
@@ -2,10 +2,12 @@ package br.com.zup.GerenciadordeContas.conta.repository;
 
 import br.com.zup.GerenciadordeContas.conta.Conta;
 import br.com.zup.GerenciadordeContas.conta.enuns.Status;
+import br.com.zup.GerenciadordeContas.conta.enuns.Tipo;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
     List<Conta> findAllByStatus(Status status);
+    List<Conta> findAllByTipo(Tipo tipo);
 }
