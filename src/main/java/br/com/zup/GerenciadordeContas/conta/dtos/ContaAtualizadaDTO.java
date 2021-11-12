517d287ce3b30ecdff04607d4d66b59ea8ba2ca4
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-12 11:41:06 -0300

    Modelar classe.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/dtos/ContaAtualizadaDTO.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/dtos/ContaAtualizadaDTO.java
@@ -1,4 +1,12 @@
 package br.com.zup.GerenciadordeContas.conta.dtos;
 
+import br.com.zup.GerenciadordeContas.conta.enuns.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter
 public class ContaAtualizadaDTO {
+    private Status status;
 }
