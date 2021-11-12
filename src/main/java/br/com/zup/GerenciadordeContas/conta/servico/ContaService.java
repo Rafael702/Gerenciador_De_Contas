a624ff064c34d98c56e0277c0e6d0a5954b6ded4
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-11 23:11:07 -0300

    Indentar código.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/servico/ContaService.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/servico/ContaService.java
@@ -5,9 +5,6 @@ import br.com.zup.GerenciadordeContas.conta.repository.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @Service
 public class ContaService {
     @Autowired
