d2a7a8e0d4e94af1f7571ac358255593d9f8b649
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-11 23:10:17 -0300

    Refatorar código.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
@@ -1,7 +1,10 @@
 package br.com.zup.GerenciadordeContas.conta.controller;
 
 import br.com.zup.GerenciadordeContas.conta.Conta;
+import br.com.zup.GerenciadordeContas.conta.dtos.CadastroContaDTO;
+import br.com.zup.GerenciadordeContas.conta.dtos.ContaSaidaDTO;
 import br.com.zup.GerenciadordeContas.conta.servico.ContaService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +15,14 @@ public class ContaController {
 
     @Autowired
     private ContaService contaService;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void cadastrarConta(@RequestBody Conta conta) {
+    public ContaSaidaDTO cadastrarConta(@RequestBody CadastroContaDTO contaDTO) {
+        Conta conta = modelMapper.map(contaDTO, Conta.class);
         contaService.salvarConta(conta);
+        return modelMapper.map(conta, ContaSaidaDTO.class);
     }
 }
