9f08e2f44e0a055a097907160658352b7a2b72a2
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-11 22:43:59 -0300

    Adicionar anotações do lombok na classe Conta.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/Conta.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/Conta.java
@@ -2,20 +2,19 @@ package br.com.zup.GerenciadordeContas.conta;
 
 import br.com.zup.GerenciadordeContas.conta.enuns.Status;
 import br.com.zup.GerenciadordeContas.conta.enuns.Tipo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "contas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Conta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,62 +29,4 @@ public class Conta {
     @Enumerated(EnumType.STRING)
     private Status status;
 
-    public Conta() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public LocalDate getDataDeVencimento() {
-        return dataDeVencimento;
-    }
-
-    public void setDataDeVencimento(LocalDate dataDeVencimento) {
-        this.dataDeVencimento = dataDeVencimento;
-    }
-
-    public LocalDateTime getDataDePagamento() {
-        return dataDePagamento;
-    }
-
-    public void setDataDePagamento(LocalDateTime dataDePagamento) {
-        this.dataDePagamento = dataDePagamento;
-    }
-
-    public Tipo getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(Tipo tipo) {
-        this.tipo = tipo;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
 }
