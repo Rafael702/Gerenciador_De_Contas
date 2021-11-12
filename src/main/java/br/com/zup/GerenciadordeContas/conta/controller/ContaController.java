2ddc8355262dd53c7d1cee0d31ea3234c111139a
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-11 23:44:44 -0300

    Adicionar método para mostrar todas as contas para o usuário.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
@@ -3,11 +3,15 @@ package br.com.zup.GerenciadordeContas.conta.controller;
 import br.com.zup.GerenciadordeContas.conta.Conta;
 import br.com.zup.GerenciadordeContas.conta.dtos.CadastroContaDTO;
 import br.com.zup.GerenciadordeContas.conta.dtos.ContaSaidaDTO;
+import br.com.zup.GerenciadordeContas.conta.dtos.ResumoContaDTO;
 import br.com.zup.GerenciadordeContas.conta.servico.ContaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/contas")
@@ -24,5 +28,16 @@ public class ContaController {
         Conta conta = modelMapper.map(contaDTO, Conta.class);
         contaService.salvarConta(conta);
         return modelMapper.map(conta, ContaSaidaDTO.class);
+    }
+
+    @GetMapping
+    public List<ResumoContaDTO> exibirContasCadastradas() {
+        List<ResumoContaDTO> listaDeContas = new ArrayList<>();
+
+        for (Conta referencia : contaService.exibirTodasAsContas()) {
+            ResumoContaDTO resumoContaDTO = modelMapper.map(referencia, ResumoContaDTO.class);
+            listaDeContas.add(resumoContaDTO);
+        }
+        return listaDeContas;
     }
 }
