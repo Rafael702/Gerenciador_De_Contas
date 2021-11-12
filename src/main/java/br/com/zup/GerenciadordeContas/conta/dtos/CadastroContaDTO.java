0afc5eb712012a5c59a7418c5885533ddbf1c37e
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-12 16:00:14 -0300

    Adicionar a validação dos campos.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/dtos/CadastroContaDTO.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/dtos/CadastroContaDTO.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @NoArgsConstructor
@@ -13,8 +17,14 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class CadastroContaDTO {
+    @Size(min = 2, max = 30, message = "O Nome Deve conter no minimo 2 Caracteres")
+    @NotBlank(message = "Nome é um campo que obrigatoriamente deve ser preenchido.")
     private String nome;
+    @Min(value = 1, message = "O Valor desta conta deve ser de no minimo $1,00")
+    @NotNull(message = " Valor é um campo que obrigatoriamente deve ser preenchido.")
     private double valor;
+    @NotNull(message = "Tipo é um campo que obrigatoriamente deve ser preenchido.")
     private Tipo tipo;
+    @NotNull(message = "Data de Vencimento é um campo que obrigatoriamente deve ser preenchido.")
     private LocalDate dataDeVencimento;
 }
