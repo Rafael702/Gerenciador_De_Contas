f9e9c42c2a18376b0fb66f40d2b5fa9cc92a1bd3
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-11 23:31:11 -0300

    Modelar classe.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/dtos/ResumoContaDTO.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/dtos/ResumoContaDTO.java
@@ -1,4 +1,18 @@
 package br.com.zup.GerenciadordeContas.conta.dtos;
 
+import br.com.zup.GerenciadordeContas.conta.enuns.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ResumoContaDTO {
+    private int id;
+    private String nome;
+    private double valor;
+    private Status status;
 }
