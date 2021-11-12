71dac6e6d1420c32c6196c0e12850adbb6fcb183
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-11 23:00:25 -0300

    Adicionar método modelMapper.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/componentes/Conversor.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/componentes/Conversor.java
@@ -1,2 +1,15 @@
-package br.com.zup.GerenciadordeContas.conta.componentes;public class Conversor {
+package br.com.zup.GerenciadordeContas.conta.componentes;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 }
