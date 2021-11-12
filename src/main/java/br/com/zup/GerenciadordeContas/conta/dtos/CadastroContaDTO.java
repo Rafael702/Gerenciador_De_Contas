92705644d08b29a17d7682efb1f461fc1e4ac1fe
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-11 23:09:50 -0300

    Modelar classe.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/dtos/CadastroContaDTO.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/dtos/CadastroContaDTO.java
@@ -1,2 +1,20 @@
-package br.com.zup.GerenciadordeContas.conta.dtos;public class CadastroContaDTO {
+package br.com.zup.GerenciadordeContas.conta.dtos;
+
+import br.com.zup.GerenciadordeContas.conta.enuns.Tipo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CadastroContaDTO {
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
 }
