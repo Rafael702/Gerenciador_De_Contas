dc64e033d9a214643dc9d66526a30748bfd402b2
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-12 17:57:29 -0300

    Adicionar a busca pelo status da conta.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
@@ -1,8 +1,11 @@
 package br.com.zup.GerenciadordeContas.conta.repository;
 
 import br.com.zup.GerenciadordeContas.conta.Conta;
+import br.com.zup.GerenciadordeContas.conta.enuns.Status;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ContaRepository extends CrudRepository<Conta, Integer> {
+import java.util.List;
 
+public interface ContaRepository extends CrudRepository<Conta, Integer> {
+    List<Conta> findAllByStatus(Status status);
 }
