611e5e857f92ca452e34d8bc81669579e2808f5e
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-12 11:42:33 -0300

    Criar método atualizarConta.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/controller/ContaController.java
@@ -1,14 +1,17 @@
 package br.com.zup.GerenciadordeContas.conta.controller;
 
 import br.com.zup.GerenciadordeContas.conta.Conta;
+import br.com.zup.GerenciadordeContas.conta.dtos.ContaAtualizadaDTO;
 import br.com.zup.GerenciadordeContas.conta.dtos.CadastroContaDTO;
 import br.com.zup.GerenciadordeContas.conta.dtos.ContaSaidaDTO;
 import br.com.zup.GerenciadordeContas.conta.dtos.ResumoContaDTO;
+import br.com.zup.GerenciadordeContas.conta.enuns.Status;
 import br.com.zup.GerenciadordeContas.conta.servico.ContaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,5 +42,16 @@ public class ContaController {
             listaDeContas.add(resumoContaDTO);
         }
         return listaDeContas;
+    }
+
+    @PutMapping("/{id}")
+    public ContaSaidaDTO atualizarConta(@PathVariable int id, @RequestBody ContaAtualizadaDTO atualizarContaDTO) {
+        Conta conta = contaService.atualizarConta(id);
+        conta.setStatus(atualizarContaDTO.getStatus());
+
+        if (conta.getStatus().equals(Status.PAGO)) {
+            return modelMapper.map(conta, ContaSaidaDTO.class);
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Esta opção é para pagar contas apenas");
     }
 }
