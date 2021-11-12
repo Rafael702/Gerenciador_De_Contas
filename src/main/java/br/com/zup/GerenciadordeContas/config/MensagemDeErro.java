334dfcc5ac1199027f2519961d3b24a0430b60b4
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-12 15:12:58 -0300

    Modelar classe.

--- src/main/java/br/com/zup/GerenciadordeContas/config/MensagemDeErro.java
+++ src/main/java/br/com/zup/GerenciadordeContas/config/MensagemDeErro.java
@@ -1,2 +1,14 @@
-package br.com.zup.GerenciadordeContas.config;public class MensagemDeErro {
+package br.com.zup.GerenciadordeContas.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class MensagemDeErro {
+    public String mensagem;
+
+
 }
