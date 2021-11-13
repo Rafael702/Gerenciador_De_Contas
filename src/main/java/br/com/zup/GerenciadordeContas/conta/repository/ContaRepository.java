8d4d54a545cb5b82b39328feec1b6cf3c4a54433
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-12 23:39:18 -0300

    Adicionar a consulta pelos valores.

--- src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadordeContas/conta/repository/ContaRepository.java
@@ -3,11 +3,16 @@ package br.com.zup.GerenciadordeContas.conta.repository;
 import br.com.zup.GerenciadordeContas.conta.Conta;
 import br.com.zup.GerenciadordeContas.conta.enuns.Status;
 import br.com.zup.GerenciadordeContas.conta.enuns.Tipo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
     List<Conta> findAllByStatus(Status status);
+
     List<Conta> findAllByTipo(Tipo tipo);
+
+    @Query(value = "SELECT * FROM contas WHERE valor BETWEEN :valor -10 AND :valor+10", nativeQuery = true)
+    List<Conta> findByValor(Double valor);
 }
